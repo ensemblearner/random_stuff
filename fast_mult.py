20730dc0256b88f37794c1fb51503ef778f201a1
Mohit <mohit1007@gmail.com>
2014-12-24 00:30:04 -0800

    monte carlo approx

--- fast_mult.py
+++ fast_mult.py
@@ -8,11 +8,36 @@ def vec_norm(vec):
     return np.linalg.norm(vec)
 
 def compute_probabilities(A, B):
-    p_i = map(lambda x: vec_norm(x), A)
-    p_j = map(lambda x: vec_norm(x), B.T)
+    """
+    P_i = || A~i|| * ||B_j||/sum(|| A~i|| * ||B_j||)
+    """
+    p_i = map(lambda x: vec_norm(x), A.T)
+    p_j = map(lambda x: vec_norm(x), B)
     probs = map(lambda x,y: x*y, p_i, p_j)
     return probs/(sum(p_i)*sum(p_j))
 
-A = np.random.randn(20, 2)
-B = np.random.randn(2, 20)
+def approximate_matrices(A, B, probs, n, c):
+    S = np.zeros((n, c))
+    D = np.zeros((c, c))
+    normed_probs = probs/sum(probs)
+    for t in range(c):
+        prob = np.random.choice(probs, p = normed_probs)
+        index = np.where(probs==prob)[0][0]
+        D[t][t] = 1/np.sqrt(c*prob)
+        S[index][t] = 1
+
+    C = np.dot(np.dot(A, S), D)
+    R = np.dot(np.dot(D, S.T), B)
+    return C, R
+m = 20
+n = 4
+d = 20
+c = 2
+A = np.random.randn(m, n)
+B = np.random.randn(n, d)
 sampling_probs = compute_probabilities(A, B)
+
+C, R = approximate_matrices(A, B, sampling_probs, n, c)
+approx_prod = np.dot(C, R)
+true_prod = np.dot(A, B)
+
